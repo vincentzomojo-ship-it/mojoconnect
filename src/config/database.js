2ec3efee2f02eeefa92fc0752ba913a9a640b18d
Mojo <mojo@Mojos-MacBook-Air.local>
2026-04-01 05:39:23 +0000

    Support DB_PORT for hosted MySQL

--- src/config/database.js
+++ src/config/database.js
@@ -6,6 +6,7 @@ const sequelize = new Sequelize(
   process.env.DB_PASS || '',
   {
     host: process.env.DB_HOST || 'localhost',
+    port: Number(process.env.DB_PORT || 3306),
     dialect: 'mysql',
     logging: false,
   }
